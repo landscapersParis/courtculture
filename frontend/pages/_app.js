26b3ebc3a00f577f25f470ba42ce1a741fb35f81
nascvi <vaiiho.nascimbene@gmail.com>
2025-08-13 23:18:32 +0200

    logo ⚡

---
+++ frontend/pages/_app.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>CourtCulture</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
